9cb487e2753057adf399dd78a23b7265b359f5cb
Galcedion <galcedion@gmx.de>
2025-04-26 11:42:02 +0200

    reworking server side buildup of the content parts

--- helper.php
+++ helper.php
@@ -1,30 +1,47 @@
 <?php
+use Joomla\CMS\Uri\Uri;
+use Joomla\CMS\Router\Route;
+
 class ModCategoryTagSearch
 {
-	public static function get_tag_list($category, $lang) {
+	public static function get_tag_list($g_cts_config) {
 		$db = JFactory::getDbo();
 		$query = $db->getQuery(true)->select('t.id, t.title, t.description')->from($db->quoteName('#__tags', 't'));
 		$query->join('LEFT', $db->quoteName('#__contentitem_tag_map', 'ctm') . ' ON ' . $db->quoteName('t.id') . '=' .  $db->quoteName('ctm.tag_id'));
 		$query->join('LEFT', $db->quoteName('#__content', 'c') . ' ON ' . $db->quoteName('ctm.content_item_id') . '=' .  $db->quoteName('c.id'));
-		$query->where('c.catid = ' . $db->quote($category));
+		$query->where('c.catid = ' . $db->quote($g_cts_config['category']));
 		$query->where('t.published = 1');
 		$query->where('c.state = 1');
-		$query->where('t.language = ' . $db->quote($lang));
-		$query->order('t.title ASC');
+		#$query->where('t.language = ' . $db->quote($g_cts_config['current_lang'])); // TODO lang should be optional!
 		$query->group('t.id');
+		$query->order('t.title ASC');
 		$db->setQuery($query);
-		return $db->loadAssocList();
+		$results = $db->loadAssocList();
+		return ModCategoryTagSearch::strip_tag_list($g_cts_config, $results);
+	}
+
+	private static function strip_tag_list($g_cts_config, $tag_list) {
+		foreach($tag_list as &$t) {
+			#$t['title'] = str_replace('-' . $g_cts_config['current_lang'], '', $t['title']); // TODO lang should be optional!
+			if($g_cts_config['striptag'] != "")
+				$t['title'] = str_replace($g_cts_config['striptag'], $t['title']);
+			$t['description'] = str_replace('<p>', '', str_replace('</p>', '', $t['description']));
+		}
+		unset($t);
+		return $tag_list;
 	}
 	
-	public static function get_article_list($category, $tags, $lang) {
+	public static function get_article_list($g_cts_config, $tags) {
+		$tagids = ModCategoryTagSearch::get_tag_ids($tags);
 		$db = JFactory::getDbo();
 		$query = $db->getQuery(true)->select('m.title, m.path, m.link')->from($db->quoteName('#__menu', 'm'));
 		$query->where('m.published = 1');
 		$query->where('m.link LIKE "%view=article%"');
-		$query->where('m.language = ' . $db->quote($lang));
+		#$query->where('m.language = ' . $db->quote($g_cts_config['current_lang'])); // TODO lang should be optional
 		$query->order('m.id ASC');
 		$db->setQuery($query);
 		$menu = $db->loadAssocList();
+		// TODO when menu is empty, there are errors displayed since path and label are never set
 		foreach($menu as &$m) {
 			$start = strpos($m['link'], '&id=') + 4;
 			if(strpos($m['link'], '&', $start) === FALSE)
@@ -33,23 +50,28 @@ class ModCategoryTagSearch
 				$m['id'] = substr($m['link'], $start, strpos($m['link'], '&', $start) - $start);
 		}
 		unset($m);
-		$query = $db->getQuery(true)->select('c.id, c.title')->from($db->quoteName('#__content', 'c'));
+		$query = $db->getQuery(true)->select('c.id, c.title, c.alias, cat.alias')->from($db->quoteName('#__content', 'c'));
 		$query->join('LEFT', $db->quoteName('#__contentitem_tag_map', 'ctm') . ' ON ' . $db->quoteName('c.id') . '=' .  $db->quoteName('ctm.content_item_id'));
-		$query->where('c.catid = ' . $db->quote($category));
+		$query->join('LEFT', $db->quoteName('#__categories', 'cat') . ' ON ' . $db->quoteName('c.catid') . '=' .  $db->quoteName('cat.id'));
+		$query->where('c.catid = ' . $db->quote($g_cts_config['category']));
 		$query->where('c.state = 1');
-		$query->where('c.language = ' . $db->quote($lang));
-		$query->where('ctm.tag_id IN (' . implode(',', $tags) . ')');
-		$query->order('c.title ASC');
+		#$query->where('c.language = ' . $db->quote($g_cts_config['current_lang'])); // TODO lang should be optional!
+		$query->where('ctm.tag_id IN (' . implode(',', $tagids) . ')');
 		$query->group('c.id');
+		$query->order('c.title ASC');
 		$db->setQuery($query);
 		$articles = $db->loadAssocList();
-		foreach($articles as &$a) {
+		foreach($articles as &$a) {$a['id'];
 			foreach($menu as $m) {
 				if($a['id'] == $m['id']) {
 					$a['path'] = $m['path'];
 					$a['label'] = $m['title'];
 					break;
 				}
+			}
+			if(!isset($a['path'])) {
+				$a['path'] = Route::_('index.php?option=com_content&view=article&id=' . $a['id'] .'&catid=' . $g_cts_config['category']);
+				$a['label'] = $a['title'];
 			}
 			$query = $db->getQuery(true)->select('t.id')->from($db->quoteName('#__tags', 't'));
 			$query->join('LEFT', $db->quoteName('#__contentitem_tag_map', 'ctm') . ' ON ' . $db->quoteName('t.id') . '=' .  $db->quoteName('ctm.tag_id'));
@@ -64,6 +86,13 @@ class ModCategoryTagSearch
 		}
 		unset($a);
 		return $articles;
+	}
+
+	private static function get_tag_ids($tag_list) {
+		$tagids = [];
+		foreach($tag_list as $t)
+			$tagids[] = $t['id'];
+		return $tagids;
 	}
 }
 
--- mod_categorytagsearch.php
+++ mod_categorytagsearch.php
@@ -2,25 +2,34 @@
 defined('_JEXEC') or die;
 require_once dirname(__FILE__) . '/helper.php';
 
-$category = $params->get('g_category');
-$striptag = $params->get('g_striptag');
-$lang = JFactory::getLanguage()->getTag();
- 
-$tag_list = ModCategoryTagSearch::get_tag_list($category, $lang);
+$param_list_int = [
+	'category', 'show_columns', 'show_rows', 'paging'
+];
+$param_list_str = [
+	'g_class', 'striptag', 'pageheader', 'pageinfo', 'custom_css'
+];
+$g_cts_config = [];
+$g_cts_config['search_mode'] = $params->get('search_mode') == 1 ? TRUE : FALSE;
+$g_cts_config['module_head'] = $params->get('module_head') == 1 ? TRUE : FALSE;
+foreach($param_list_int as $pli) {
+	$g_cts_config[$pli] = intval($params->get($pli));
+}
+foreach($param_list_str as $pls) {
+	$g_cts_config[$pls] = $params->get($pls) === NULL ? '' : trim($params->get($pls));
+}
+if($g_cts_config['search_mode']) {
+	$g_cts_config['GET'] = [];
+	$g_cts_config['GET']['tags'] = isset($_GET['tags']) ? filter_var($_GET['tags'], FILTER_SANITIZE_URL) : NULL;
+	$g_cts_config['GET']['p'] = isset($_GET['p']) ? filter_var($_GET['p'], FILTER_SANITIZE_NUMBER_INT) : NULL;
+}
+$g_cts_config['current_lang'] = JFactory::getLanguage()->getTag();
+
+$tag_list = ModCategoryTagSearch::get_tag_list($g_cts_config);
+
 if(count($tag_list) == 0)
 	return;
-$tagids = array();
 
-foreach($tag_list as &$t) {
-	$tagids[] = $t['id'];
-	$t['title'] = str_replace('-' . $lang, '', $t['title']);
-	if($striptag != "")
-		$t['title'] = str_replace($striptag, $t['title']);
-	$t['description'] = str_replace('<p>', '', str_replace('</p>', '', $t['description']));
-}
-unset($t);
-
-$article_list = ModCategoryTagSearch::get_article_list($category, $tagids, $lang);
+$article_list = ModCategoryTagSearch::get_article_list($g_cts_config, $tag_list);
 
 require JModuleHelper::getLayoutPath('mod_categorytagsearch');
 
